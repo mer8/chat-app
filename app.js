e17f3cda42f876d7e1f1cb8c5abf501d8f300879
Meredith Bryan <merbryan@gmail.com>
2014-09-04 19:48:25 -0700

    Replace jade with hogan. Add new html syntax for handling errors in error.html

--- app.js
+++ app.js
@@ -26,8 +26,11 @@ var users = require('./routes/users');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
-app.set('view engine', 'jade');
+// Remove this below and replace with hogan engine
+// app.set('view engine', 'jade');
+app.set('view engine', 'hjs');
+app.engine('html', require('hogan-express'));
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
